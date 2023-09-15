8a4f2898c5282a10b07fd1549dbcdbf4b720d535
mari-s4e <ricci@space4environment.com>
2023-09-15 10:36:46 +0200

    added logfiles

--- 02_processing/CWS_drought_linear_trends.py
+++ 02_processing/CWS_drought_linear_trends.py
@@ -6,8 +6,10 @@ import pandas as pd
 import numpy as np
 import pymannkendall
 import glob
+from datetime import datetime
+from loguru import logger
 
-
+@logger.catch
 def extract_trends(base_path):
 
     variables_dict = {"SMA_annual": "SMA_gs_1km_annual",
@@ -21,6 +23,7 @@ def extract_trends(base_path):
 
     # loop through each variable for which a trend analysis is carried out
     for v_name, v_path in variables_dict.items():
+        logger.info(f"compute  {v_name}")
         in_path = os.path.join(base_path, v_path)
 
         filenames = glob.glob(in_path + "/*.tif")
@@ -31,6 +34,7 @@ def extract_trends(base_path):
         v_yearly = v_yearly.assign_coords({"time": pd.to_datetime(f0.split("_")[-1].split(".")[0] + "-12-31")})
 
         for f in filenames:
+            logger.info(f"append  {f}")
             if f != f0:
                 sma_tmp = rio.open_rasterio(f)
                 sma_tmp = sma_tmp.mean("band")
@@ -56,6 +60,7 @@ def extract_trends(base_path):
                         "x": v_yearly.x.values})
         })
 
+        logger.info("compute Mann-Kendall slope and p-value")
         for i in xy_tuples:
             x_tmp, y_tmp = i[1], i[0]
             ts_tmp = v_yearly.sel(x=x_tmp, y=y_tmp).to_series()
@@ -74,19 +79,23 @@ def extract_trends(base_path):
             os.mkdir(v_out_path)
 
         # save a tif file with 2 bands: slope and p_value from Theil-Sen trend analysis
+        logger.info("save results")
         mk_statistics = mk_statistics.fillna(-999)
         mk_statistics['slope'].rio.write_nodata(-999, inplace=True)
         mk_statistics['p_value'].rio.write_nodata(-999, inplace=True)
 
         mk_statistics.rio.write_crs("epsg:3035", inplace=True)
         mk_statistics.rio.set_spatial_dims(x_dim="x", y_dim="y", inplace=True)
-        mk_statistics.transpose('y', 'x').rio.to_raster(os.path.join(v_out_path, "trend_" + v_name + ".tif"))
+        mk_statistics.transpose('y', 'x').rio.to_raster(os.path.join(v_out_path, "trend_" + v_name + ".tif"), compress='LZW')
 
     return None
 
 
 if __name__ == "__main__":
 
+    # add log file
+    logger.add("99_logfiles\logfile_SMA_trends.log")
+    
     # base_path = Path(r'C:\Users\Zappa\Documents\ETC_DI\drought_indicator\output\SMA_gs_1km_EU')
     base_path = Path(r'L:\f02_data\drought_indicator\output\SMA_gs_1km_EU')
 
